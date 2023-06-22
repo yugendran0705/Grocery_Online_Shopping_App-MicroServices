7e482fdebb95b34201d68f623ab957c1713282b3
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-22 09:55:28 +0530

    updated

---
+++ customer/src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const { port } = require('./config/index');
+const { connect } = require('./database/connection');
+const customerRoutes = require('./routes/customer');
+const { CustomerService } = require('./services/customer-service');
+
+app.use(express.json());
+app.use('/', customerRoutes);
+
+const service = new CustomerService();
+app.use('/app-events', async (req, res) => {
+    const { payload } = req.body;
+    service.subscribeEvents(payload);
+
+    console.log("Event received");
+    res.status(200).json(payload);
+});
+
+
+app.listen(port, async () => {
+    console.log(`Server started on port ${port}`);
+    if (await connect()) {
+        console.log('Database connected');
+    }
+    else {
+        console.log('Database connection failed');
+    }
+});
