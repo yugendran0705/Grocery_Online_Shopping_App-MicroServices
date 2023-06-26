9098222ae69e3d95910007c67a4900c9d1956e87
Yugendran <108662548+yugendran0705@users.noreply.github.com>
2023-06-26 17:39:58 +0530

    Update index.js

--- gateway/index.js
+++ gateway/index.js
@@ -7,9 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/shopping', proxy('http://localhost:8003'));
 app.use('/customer', proxy('http://localhost:8001'));
 app.use('/', proxy('http://localhost:8002'));
-app.use('/shopping', proxy('http://localhost:8003'));
 
 app.listen(8000, () => {
     console.log('Server is running on port 8000');
